733ea5696cac3efac86c65969290308ab2a0e763
wchorski <wchorski@gmail.com>
2026-05-12 17:14:03 -0500

    points of interet mixes geoapify and wikipedia api, it is not always accurate but works well enough

--- src/env.d.ts
+++ src/env.d.ts
@@ -18,6 +18,7 @@ interface ImportMetaEnv {
   readonly PGHOST?: string;
   readonly NODE_ENV?: "production" | "development";
   readonly DATABASE_ID_SECRET?: string;
+  readonly GEO_APIFY_KEY?: string;
 }
 
 interface ImportMeta {
